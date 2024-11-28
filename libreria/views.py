b15fe89ca8e21bff5f00e38a4f8e23ac5ee7ee4a
Your Name <evizcarra@unsa.edu.pe>
2024-11-27 21:59:21 -0500

    avanzamos las vistas que usaremos

--- libreria/views.py
+++ libreria/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ResenaForm
+from .models import Libro, Usuario, Reseña 
 
-# Create your views here.
+def listar_libros(request):
+    libros = Libro.objects.all()
+    return render(request, 'libreria/listar_libros.html', {'libros': libros})
+
+def listar_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'libreria/listar_usuarios.html', {'usuarios': usuarios})
+
+def listar_resenas(request):
+    resenas = Reseña.objects.all()
+    return render(request, 'libreria/listar_resenas.html', {'resenas': resenas})
+
+def crear_resena(request):
+    if request.method == 'POST':
+        form = ResenaForm(request.POST)
+        if form.is_valid():
+            reseña = form.save(commit=False)
+            reseña.usuario = request.user
+            reseña.save()
+            return redirect('listar_resenas')
+    else:
+        form = ResenaForm()
+
+    return render(request, 'libreria/crear_resena.html', {'form': form})
